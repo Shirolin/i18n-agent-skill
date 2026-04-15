70c5dfa9fe5e5ffe6ede567927de156801ea002c
Shirolin <shirolin.com@gmail.com>
2026-04-15 14:45:49 +0800

    chore(api): 完善 __init__.py 导出接口，补齐面向 Python SDK 的核心工具

--- i18n_agent_skill/__init__.py
+++ i18n_agent_skill/__init__.py
@@ -1,12 +1,26 @@
-from .tools import extract_raw_strings, sync_i18n_files, get_missing_keys
-from .models import ConflictStrategy, ExtractInput, SyncInput, MissingKeysInput
+from .tools import (
+    check_project_status,
+    extract_raw_strings,
+    propose_sync_i18n,
+    refine_i18n_proposal,
+    commit_i18n_changes,
+    load_project_glossary,
+    update_project_glossary,
+    get_missing_keys,
+    sync_i18n_files
+)
+from .models import ConflictStrategy, PrivacyLevel
 
 __all__ = [
+    "check_project_status",
     "extract_raw_strings",
-    "sync_i18n_files",
+    "propose_sync_i18n",
+    "refine_i18n_proposal",
+    "commit_i18n_changes",
+    "load_project_glossary",
+    "update_project_glossary",
     "get_missing_keys",
+    "sync_i18n_files",
     "ConflictStrategy",
-    "ExtractInput",
-    "SyncInput",
-    "MissingKeysInput"
+    "PrivacyLevel"
 ]
